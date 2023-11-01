7235cff456c909be17fceda9b4b08714d3555ed6
welldyrosman <welldyrosman@gmail.com>
2023-11-02 00:50:29 +0700

    slicing dashboard

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -1,5 +1,201 @@
 <x-app-layout>
-    <x-slot name="title">{{ $title??"" }}</x-slot>
+    <x-slot name="title">{{ $title ?? '' }}</x-slot>
     <section>
+        <div class="row">
+           
+            <div class="col-12">
+                <div class="card mt-5">
+                    
+                    <div class="card-body p-0 position-relative">
+                        
+                        <div class="card-p mt-n20">
+                            <div class="row">
+                                <div class="col bg-light-warning px-4 py-8 rounded-2 me-7 mb-7">
+                                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
+                                    <div class="d-flex align-items-end">
+
+                                        <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none">
+                                                <path opacity="0.3"
+                                                    d="M20.0381 4V10C20.0381 10.6 19.6381 11 19.0381 11H17.0381C16.4381 11 16.0381 10.6 16.0381 10V4C16.0381 2.9 16.9381 2 18.0381 2C19.1381 2 20.0381 2.9 20.0381 4ZM9.73808 18.9C10.7381 18.5 11.2381 17.3 10.8381 16.3L5.83808 3.29999C5.43808 2.29999 4.23808 1.80001 3.23808 2.20001C2.23808 2.60001 1.73809 3.79999 2.13809 4.79999L7.13809 17.8C7.43809 18.6 8.23808 19.1 9.03808 19.1C9.23808 19 9.53808 19 9.73808 18.9ZM19.0381 18H17.0381V20H19.0381V18Z"
+                                                    fill="black" />
+                                                <path
+                                                    d="M18.0381 6H4.03809C2.93809 6 2.03809 5.1 2.03809 4C2.03809 2.9 2.93809 2 4.03809 2H18.0381C19.1381 2 20.0381 2.9 20.0381 4C20.0381 5.1 19.1381 6 18.0381 6ZM4.03809 3C3.43809 3 3.03809 3.4 3.03809 4C3.03809 4.6 3.43809 5 4.03809 5C4.63809 5 5.03809 4.6 5.03809 4C5.03809 3.4 4.63809 3 4.03809 3ZM18.0381 3C17.4381 3 17.0381 3.4 17.0381 4C17.0381 4.6 17.4381 5 18.0381 5C18.6381 5 19.0381 4.6 19.0381 4C19.0381 3.4 18.6381 3 18.0381 3ZM12.0381 17V22H6.03809V17C6.03809 15.3 7.33809 14 9.03809 14C10.7381 14 12.0381 15.3 12.0381 17ZM9.03809 15.5C8.23809 15.5 7.53809 16.2 7.53809 17C7.53809 17.8 8.23809 18.5 9.03809 18.5C9.83809 18.5 10.5381 17.8 10.5381 17C10.5381 16.2 9.83809 15.5 9.03809 15.5ZM15.0381 15H17.0381V13H16.0381V8L14.0381 10V14C14.0381 14.6 14.4381 15 15.0381 15ZM19.0381 15H21.0381C21.6381 15 22.0381 14.6 22.0381 14V10L20.0381 8V13H19.0381V15ZM21.0381 20H15.0381V22H21.0381V20Z"
+                                                    fill="black" />
+                                            </svg></span>
+                                        <h1 class="fw-bolder text-warning text-center m-0" style="font-size: 3rem">2</h1>
+
+                                    </div>
+
+                                    <!--end::Svg Icon-->
+                                    <a href="#" class="text-warning fw-bold fs-6">Pekerjaan Konstruksi</a>
+
+                                </div>
+                                <div class="col bg-light-warning px-4 py-8 rounded-2 me-7 mb-7">
+                                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
+                                    <div class="d-flex align-items-end">
+
+                                        <span class="svg-icon svg-icon-3x svg-icon-warning d-block my-2"><svg
+                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                                viewBox="0 0 24 24" fill="none">
+                                                <path opacity="0.3"
+                                                    d="M20.0381 4V10C20.0381 10.6 19.6381 11 19.0381 11H17.0381C16.4381 11 16.0381 10.6 16.0381 10V4C16.0381 2.9 16.9381 2 18.0381 2C19.1381 2 20.0381 2.9 20.0381 4ZM9.73808 18.9C10.7381 18.5 11.2381 17.3 10.8381 16.3L5.83808 3.29999C5.43808 2.29999 4.23808 1.80001 3.23808 2.20001C2.23808 2.60001 1.73809 3.79999 2.13809 4.79999L7.13809 17.8C7.43809 18.6 8.23808 19.1 9.03808 19.1C9.23808 19 9.53808 19 9.73808 18.9ZM19.0381 18H17.0381V20H19.0381V18Z"
+                                                    fill="black" />
+                                                <path
+                                                    d="M18.0381 6H4.03809C2.93809 6 2.03809 5.1 2.03809 4C2.03809 2.9 2.93809 2 4.03809 2H18.0381C19.1381 2 20.0381 2.9 20.0381 4C20.0381 5.1 19.1381 6 18.0381 6ZM4.03809 3C3.43809 3 3.03809 3.4 3.03809 4C3.03809 4.6 3.43809 5 4.03809 5C4.63809 5 5.03809 4.6 5.03809 4C5.03809 3.4 4.63809 3 4.03809 3ZM18.0381 3C17.4381 3 17.0381 3.4 17.0381 4C17.0381 4.6 17.4381 5 18.0381 5C18.6381 5 19.0381 4.6 19.0381 4C19.0381 3.4 18.6381 3 18.0381 3ZM12.0381 17V22H6.03809V17C6.03809 15.3 7.33809 14 9.03809 14C10.7381 14 12.0381 15.3 12.0381 17ZM9.03809 15.5C8.23809 15.5 7.53809 16.2 7.53809 17C7.53809 17.8 8.23809 18.5 9.03809 18.5C9.83809 18.5 10.5381 17.8 10.5381 17C10.5381 16.2 9.83809 15.5 9.03809 15.5ZM15.0381 15H17.0381V13H16.0381V8L14.0381 10V14C14.0381 14.6 14.4381 15 15.0381 15ZM19.0381 15H21.0381C21.6381 15 22.0381 14.6 22.0381 14V10L20.0381 8V13H19.0381V15ZM21.0381 20H15.0381V22H21.0381V20Z"
+                                                    fill="black" />
+                                            </svg></span>
+                                        <h1 class="fw-bolder text-warning text-center m-0" style="font-size: 3rem">2</h1>
+
+                                    </div>
+
+                                    <!--end::Svg Icon-->
+                                    <a href="#" class="text-warning fw-bold fs-6">Pekerjaan Konstruksi</a>
+
+                                </div>
+                                <div class="col bg-light-primary px-4 py-8 rounded-2 me-7 mb-7">
+                                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
+                                    <div class="d-flex align-items-end">
+
+                                        <span class="svg-icon svg-icon-3x svg-icon-primary d-block my-2">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                                <path opacity="0.3" d="M20 15H4C2.9 15 2 14.1 2 13V7C2 6.4 2.4 6 3 6H21C21.6 6 22 6.4 22 7V13C22 14.1 21.1 15 20 15ZM13 12H11C10.5 12 10 12.4 10 13V16C10 16.5 10.4 17 11 17H13C13.6 17 14 16.6 14 16V13C14 12.4 13.6 12 13 12Z" fill="black"></path>
+                                                <path d="M14 6V5H10V6H8V5C8 3.9 8.9 3 10 3H14C15.1 3 16 3.9 16 5V6H14ZM20 15H14V16C14 16.6 13.5 17 13 17H11C10.5 17 10 16.6 10 16V15H4C3.6 15 3.3 14.9 3 14.7V18C3 19.1 3.9 20 5 20H19C20.1 20 21 19.1 21 18V14.7C20.7 14.9 20.4 15 20 15Z" fill="black"></path>
+                                            </svg>
+                                        </span>
+                                        <h1 class="fw-bolder text-primary text-center m-0" style="font-size: 3rem">2</h1>
+
+                                    </div>
+
+                                    <!--end::Svg Icon-->
+                                    <a href="#" class="text-primary fw-bold fs-6">Jasa Lainnya</a>
+
+                                </div>
+                                <div class="col bg-light-success px-4 py-8 rounded-2 me-7 mb-7">
+                                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
+                                    <div class="d-flex align-items-end">
+
+                                        <span class="svg-icon svg-icon-3x svg-icon-success d-block my-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path opacity="0.3" d="M11.425 7.325C12.925 5.825 15.225 5.825 16.725 7.325C18.225 8.825 18.225 11.125 16.725 12.625C15.225 14.125 12.925 14.125 11.425 12.625C9.92501 11.225 9.92501 8.825 11.425 7.325ZM8.42501 4.325C5.32501 7.425 5.32501 12.525 8.42501 15.625C11.525 18.725 16.625 18.725 19.725 15.625C22.825 12.525 22.825 7.425 19.725 4.325C16.525 1.225 11.525 1.225 8.42501 4.325Z" fill="black"/>
+                                            <path d="M11.325 17.525C10.025 18.025 8.425 17.725 7.325 16.725C5.825 15.225 5.825 12.925 7.325 11.425C8.825 9.92498 11.125 9.92498 12.625 11.425C13.225 12.025 13.625 12.925 13.725 13.725C14.825 13.825 15.925 13.525 16.725 12.625C17.125 12.225 17.425 11.825 17.525 11.325C17.125 10.225 16.525 9.22498 15.625 8.42498C12.525 5.32498 7.425 5.32498 4.325 8.42498C1.225 11.525 1.225 16.625 4.325 19.725C7.425 22.825 12.525 22.825 15.625 19.725C16.325 19.025 16.925 18.225 17.225 17.325C15.425 18.125 13.225 18.225 11.325 17.525Z" fill="black"/>
+                                            </svg></span>
+                                        <h1 class="fw-bolder text-success text-center m-0" style="font-size: 3rem">2</h1>
+
+                                    </div>
+
+                                    <!--end::Svg Icon-->
+                                    <a href="#" class="text-success fw-bold fs-6">Jasa Konsultasi</a>
+
+                                </div>
+                                <div class="col bg-light-info px-4 py-8 rounded-2 mb-7">
+                                    <!--begin::Svg Icon | path: icons/duotune/general/gen032.svg-->
+                                    <div class="d-flex align-items-end">
+
+                                        <span class="svg-icon svg-icon-3x svg-icon-info d-block my-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path opacity="0.3" d="M3 13H10C10.6 13 11 13.4 11 14V21C11 21.6 10.6 22 10 22H3C2.4 22 2 21.6 2 21V14C2 13.4 2.4 13 3 13Z" fill="black"/>
+                                            <path d="M7 16H6C5.4 16 5 15.6 5 15V13H8V15C8 15.6 7.6 16 7 16Z" fill="black"/>
+                                            <path opacity="0.3" d="M14 13H21C21.6 13 22 13.4 22 14V21C22 21.6 21.6 22 21 22H14C13.4 22 13 21.6 13 21V14C13 13.4 13.4 13 14 13Z" fill="black"/>
+                                            <path d="M18 16H17C16.4 16 16 15.6 16 15V13H19V15C19 15.6 18.6 16 18 16Z" fill="black"/>
+                                            <path opacity="0.3" d="M3 2H10C10.6 2 11 2.4 11 3V10C11 10.6 10.6 11 10 11H3C2.4 11 2 10.6 2 10V3C2 2.4 2.4 2 3 2Z" fill="black"/>
+                                            <path d="M7 5H6C5.4 5 5 4.6 5 4V2H8V4C8 4.6 7.6 5 7 5Z" fill="black"/>
+                                            <path opacity="0.3" d="M14 2H21C21.6 2 22 2.4 22 3V10C22 10.6 21.6 11 21 11H14C13.4 11 13 10.6 13 10V3C13 2.4 13.4 2 14 2Z" fill="black"/>
+                                            <path d="M18 5H17C16.4 5 16 4.6 16 4V2H19V4C19 4.6 18.6 5 18 5Z" fill="black"/>
+                                            </svg></span>
+                                        <h1 class="fw-bolder text-info text-center m-0" style="font-size: 3rem">2</h1>
+
+                                    </div>
+
+                                    <!--end::Svg Icon-->
+                                    <a href="#" class="text-info fw-bold fs-6">Pengadaan Barang</a>
+
+                                </div>
+                            </div>
+                            <div class="card card-flush h-lg-100">
+                                <!--begin::Card header-->
+                                <div class="card-header mt-6">
+                                    <!--begin::Card title-->
+                                    <div class="card-title flex-column">
+                                        <h3 class="fw-bolder mb-1">Tasks Summary</h3>
+                                        <div class="fs-6 fw-bold text-gray-400">24 Overdue Tasks</div>
+                                    </div>
+                                    <!--end::Card title-->
+                                    <!--begin::Card toolbar-->
+                                    <div class="card-toolbar">
+                                        <a href="#" class="btn btn-light btn-sm">View Tasks</a>
+                                    </div>
+                                    <!--end::Card toolbar-->
+                                </div>
+                                <!--end::Card header-->
+                                <!--begin::Card body-->
+                                <div class="card-body p-9 pt-5">
+                                    <!--begin::Wrapper-->
+                                    <div class="d-flex flex-wrap">
+                                        <!--begin::Chart-->
+                                        <div class="position-relative d-flex flex-center h-175px w-175px me-15 mb-7">
+                                            <div class="position-absolute translate-middle start-50 top-50 d-flex flex-column flex-center">
+                                                <span class="fs-2qx fw-bolder">237</span>
+                                                <span class="fs-6 fw-bold text-gray-400">Total Tasks</span>
+                                            </div>
+                                            <canvas id="project_overview_chart" width="350" height="350" style="display: block; box-sizing: border-box; height: 175px; width: 175px;"></canvas>
+                                        </div>
+                                        <!--end::Chart-->
+                                        <!--begin::Labels-->
+                                        <div class="d-flex flex-column justify-content-center flex-row-fluid pe-11 mb-5">
+                                            <!--begin::Label-->
+                                            <div class="d-flex fs-6 fw-bold align-items-center mb-3">
+                                                <div class="bullet bg-primary me-3"></div>
+                                                <div class="text-gray-400">Active</div>
+                                                <div class="ms-auto fw-bolder text-gray-700">30</div>
+                                            </div>
+                                            <!--end::Label-->
+                                            <!--begin::Label-->
+                                            <div class="d-flex fs-6 fw-bold align-items-center mb-3">
+                                                <div class="bullet bg-success me-3"></div>
+                                                <div class="text-gray-400">Completed</div>
+                                                <div class="ms-auto fw-bolder text-gray-700">45</div>
+                                            </div>
+                                            <!--end::Label-->
+                                            <!--begin::Label-->
+                                            <div class="d-flex fs-6 fw-bold align-items-center mb-3">
+                                                <div class="bullet bg-danger me-3"></div>
+                                                <div class="text-gray-400">Overdue</div>
+                                                <div class="ms-auto fw-bolder text-gray-700">0</div>
+                                            </div>
+                                            <!--end::Label-->
+                                            <!--begin::Label-->
+                                            <div class="d-flex fs-6 fw-bold align-items-center">
+                                                <div class="bullet bg-gray-300 me-3"></div>
+                                                <div class="text-gray-400">Yet to start</div>
+                                                <div class="ms-auto fw-bolder text-gray-700">25</div>
+                                            </div>
+                                            <!--end::Label-->
+                                        </div>
+                                        <!--end::Labels-->
+                                    </div>
+                                    <!--end::Wrapper-->
+                                    <!--begin::Notice-->
+                                    <div class="notice d-flex bg-light-primary rounded border-primary border border-dashed p-6">
+                                        <!--begin::Wrapper-->
+                                        <div class="d-flex flex-stack flex-grow-1">
+                                            <!--begin::Content-->
+                                            <div class="fw-bold">
+                                                <div class="fs-6 text-gray-700">
+                                                <a href="#" class="fw-bolder me-1">Invite New .NET Collaborators</a>to create great outstanding business to business .jsp modutr class scripts</div>
+                                            </div>
+                                            <!--end::Content-->
+                                        </div>
+                                        <!--end::Wrapper-->
+                                    </div>
+                                    <!--end::Notice-->
+                                </div>
+                                <!--end::Card body-->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     </section>
 </x-app-layout>
